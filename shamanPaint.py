97c8f929fbbbc1258e67c90e1dde16c7abdd94a5
Sam Willenson <sam.willenson@gmail.com>
2020-04-25 19:24:11 -0700

    polar time baby

--- shamanPaint.py
+++ shamanPaint.py
@@ -1,84 +1,129 @@
-import pygame, random
+import pygame
+import random
+import math
 
-pygame.init()
 
-#josh is weak here is my extra line
+# new idea--make it so I can introduce as many points as I want
+# and it will self balance where to fill the points in symmetrically
 
-#new idea--make it so I can introduce as many points as I want and it will self balance where to fill the points in symmetrically
+shadowClones = 10
 
-draw = False
-oldCoord = (0,0)
-oldxFlipCoords = (0,0)
-oldyFlipCoords = (0,0)
-oldxyFlipCoords = (0,0)
-color = (0,0,0)
-lineThicc = 3
-thickness = 10
-if thickness == 10:
-    lineThicc = 4
-if thickness == 5:
-    lineThicc = 2
 screenWidth = 1000
 screenHeight = 1000
 
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
-def line (startCoord, endCoord, color, thickness, canvas):
-    dx = endCoord[0] - startCoord[0]
-    dy = endCoord[1] - startCoord[1]
-    longer = max(abs(dx), abs(dy))                                              #here is the def for sweep of circles lines
-    for i in range (longer):
-        x = int(startCoord[0]+(i/longer)*dx)
-        y = int(startCoord[1]+(i/longer)*dy)
-        pygame.draw.circle(screen, color, (x,y), thickness)
 
-def xFlipCoord (coord):                                                         #get new coordinates for the reflected lines
-    newX = int(screenWidth-coord[0])
-    newY = coord[1]
-    return (newX, newY)
+def line(startCoord, endCoord, color, thickness, canvas):
+    dx = int(endCoord[0] - startCoord[0])
+    dy = int(endCoord[1] - startCoord[1])
+    # here is the def for sweep of circles lines
+    longer = max(abs(dx), abs(dy))
+    for i in range (int(longer)):
+        x = int(startCoord[0] + (i / longer) * dx)
+        y = int(startCoord[1] + (i / longer) * dy)
+        pygame.draw.circle(screen, color, (x, y), thickness)
 
-def yFlipCoord (coord):                                                         #get new coordinates for the reflected lines
-    newX = coord[0]
-    newY = int(screenHeight-coord[1])
-    return (newX, newY)
+def makePolar(coord):
+    """
+    turn cartesian coords into polar coordinates in degrees
+    """
+    dx = (coord[0] - screenWidth/2)
+    dy = (coord[1] - screenHeight/2)
+    if dx == 0:
+        rho = int(math.sqrt((dx**2) + (dy**2)))
+        theta = 90
+        return (rho, theta)
+    rho = int(math.sqrt((dx**2) + (dy**2)))
+    theta = math.atan(dy / dx)
+    return (rho, math.degrees(theta))
 
-def xyFlipCoord (coord):                                                         #get new coordinates for the reflected lines
-    newX = int(screenWidth-coord[0])
-    newY = int(screenHeight-coord[1])
-    return (newX, newY)
+def makeCartesian(rho, theta):
+    """
+    turn polar coordinates in degrees into cartesian
+    """
+    dx = rho * math.cos(math.radians(theta))
+    dy = rho * math.sin(math.radians(theta))
+    x = abs(screenWidth / 2 - dx)
+    y = abs(screenHeight / 2 - dy)
+    return (x,y)
+
+def summonShadowClones(coord, replicas):
+    """
+    draw all desired shadow summonShadowClones
+    """
+    thetaBank = []
+    cartesianBank = []
+    r, t = makePolar(coord)
+
+    if coord[0] > screenWidth / 2:
+            t += 180
+
+    # print("before r: ", r, "before t: ", t)
+    for i in range(replicas):
+        t +=  360/replicas
+        # print ("i is: ", i)
+        # print(" after r: ", r, "after t: ", t)
+
+        cartesianBank.append(makeCartesian(r, t))
+
+    # print ("cartbank is: ", cartesianBank)
+    return (cartesianBank)
 
 
-while True:
-    e = pygame.event.wait()
-    if e.type == pygame.QUIT:
-        pygame.quit()
-        break
-    if e.type == pygame.MOUSEBUTTONDOWN:
-        draw = True
-        color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-    if e.type == pygame.MOUSEBUTTONUP:
-        draw = False
-    if e.type == pygame.MOUSEMOTION:
-        if draw == True:
-            pygame.draw.circle(screen, color, e.pos, lineThicc)
-            line (e.pos, oldCoord, color, thickness, screen)                    #these are both options: this one is my own def using sweep of circles
+def main():
 
-            xFlipCoords = xFlipCoord (e.pos)
-            pygame.draw.circle(screen, color, xFlipCoords, lineThicc)
-            line (xFlipCoords, oldxFlipCoords, color, thickness, screen)
+    draw = False
+    oldCoord = (420.69, 420.69)
+    shadowOld = []
+    shadow = []
+    color = (0, 0, 0)
+    lineThicc = 3
+    thickness = 10
 
-            yFlipCoords = yFlipCoord (e.pos)
-            pygame.draw.circle(screen, color, yFlipCoords, lineThicc)
-            line (yFlipCoords, oldyFlipCoords, color, thickness, screen)
+    if thickness == 10:
+        lineThicc = 4
+    if thickness == 5:
+        lineThicc = 2
 
-            xyFlipCoords = xyFlipCoord (e.pos)
-            pygame.draw.circle(screen, color, xyFlipCoords, lineThicc)
-            line (xyFlipCoords, oldxyFlipCoords, color, thickness, screen)
+    pygame.init()
+
+    while True:
+        e = pygame.event.wait()
+
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            break
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            draw = True
+            color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+        elif e.type == pygame.MOUSEBUTTONUP:
+            draw = False
+        elif e.type == pygame.MOUSEMOTION:
+            if draw == True:
+                if oldCoord == (420.69, 420.69):
+                    continue
+
+                line(e.pos, oldCoord, color, thickness, screen)
+
+                shadow = summonShadowClones(e.pos, shadowClones)
+
+                for i in range(shadowClones):
+                    if oldCoord == (420.69, 420.69):
+                        continue
+                    line(shadow[i], shadowOld[i], color, thickness, screen)
 
 
-            # pygame.draw.line(screen, color, e.pos, oldCoord, thickness)       #these are both options: this one is pygame provided using rectangles
-        oldCoord = e.pos
-        oldxFlipCoords = xFlipCoord (e.pos)
-        oldyFlipCoords = yFlipCoord (e.pos)
-        oldxyFlipCoords = xyFlipCoord (e.pos)
-    pygame.display.flip()
+            oldCoord = e.pos
+            shadowOld = summonShadowClones(e.pos, shadowClones)
+
+            # for i in range(len(cartesianBank)):
+
+            # shadow1Old = summonShadowClone1(e.pos, shadowClones)
+
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
